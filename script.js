700a8db35c83536d3dddd673c36b1cc74d302716
Nidhishah74 <125784326+Nidhishah74@users.noreply.github.com>
2023-04-14 17:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+function removeColor() {
+    var select = document.getElementById("colorSelect");
+    var selectedOption = select.options[select.selectedIndex];
+    if (selectedOption) {
+      select.removeChild(selectedOption);
+    }
+  }
 
